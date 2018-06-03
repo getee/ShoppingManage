897674dcb8d75c3d14ed60927691df736e322901
getee <634630676@qq.com>
2018-06-03 14:56:23 +0800

    使用新的数据库结构

--- src/com/getee/shopmanage/model/dao/UserDAO.java
+++ src/com/getee/shopmanage/model/dao/UserDAO.java
@@ -29,6 +29,11 @@ public interface UserDAO {
      * 分页获取,按用戶名搜索用户信息
      */
     public ArrayList<User> searchNameUser(String name,int page, int count);// 按%name%条件搜索用户
+    
+    /**
+     * 分页获取,按城市搜索用户信息
+     */
+    public ArrayList<User> searchProvinceUser(String name,int page, int count);// 按城市条件搜索用户
 
     /**
      * 新添加用户数据(id会自动增长)
