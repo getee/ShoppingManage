3f72846142f7dd3cd4b7470a36c98aae5977bc6e
Crunrain <954773233@qq.com>
2018-06-03 15:10:56 +0800

    searchDD

--- WebContent/WEB-INF/myshop.sql
+++ WebContent/WEB-INF/myshop.sql
@@ -2,6 +2,7 @@ SET SESSION FOREIGN_KEY_CHECKS=0;
 
 /* Drop Tables */
 
+DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS dingdan;
 DROP TABLE IF EXISTS shopcar;
 DROP TABLE IF EXISTS users;
@@ -9,15 +10,38 @@ DROP TABLE IF EXISTS wine;
 
 
 
+
 /* Create Tables */
+
+CREATE TABLE comments
+(
+	-- 评论id
+	com_id int NOT NULL AUTO_INCREMENT COMMENT '评论id',
+	user_id int NOT NULL,
+	wine_id int NOT NULL,
+	-- 0 好评
+	-- 1 中评
+	-- 2 差评
+	state int DEFAULT 0,
+	content text,
+	PRIMARY KEY (com_id)
+);
+
 
 CREATE TABLE dingdan
 (
 	dd_id int NOT NULL AUTO_INCREMENT,
 	user_id int NOT NULL,
-	wine_id int NOT NULL,
+	-- 把所有商品id都添加用";"分割.例：002;092
+	wines_ids varchar(99) NOT NULL COMMENT '把所有商品id都添加用";"分割.例：002;092',
+	-- 0 待发货
+	-- 1 快递中
+	-- 2 已送达
+	state int DEFAULT 0 NOT NULL ,
 	-- 购买数量
 	number int DEFAULT 1 NOT NULL COMMENT '购买数量',
+	-- 总价格
+	prices float NOT NULL COMMENT '总价格',
 	PRIMARY KEY (dd_id)
 );
 
@@ -33,13 +57,15 @@ CREATE TABLE shopcar
 );
 
 
-
 CREATE TABLE users
 (
 	user_id int NOT NULL AUTO_INCREMENT,
 	user_name varchar(20) NOT NULL,
 	password varchar(99) NOT NULL,
-	address varchar(20),
+	-- 省
+	province varchar(20) COMMENT '省',
+	-- 市
+	city varchar(20) COMMENT '市',
 	phone int(11),
 	picture varchar(99),
 	PRIMARY KEY (user_id),
@@ -54,13 +80,24 @@ CREATE TABLE wine
 	kind varchar(20) NOT NULL,
 	price float NOT NULL,
 	detail text,
-	picture varchar(50) NOT NULL,
+	-- 封面大图
+	picture varchar(99) NOT NULL COMMENT '封面大图',
+	-- 剩下四张图
+	picture4 varchar(99) COMMENT '剩下四张图',
 	PRIMARY KEY (wine_id)
 );
 
 
 
 /* Create Foreign Keys */
+
+ALTER TABLE comments
+	ADD FOREIGN KEY (user_id)
+	REFERENCES users (user_id)
+	ON UPDATE RESTRICT
+	ON DELETE RESTRICT
+;
+
 
 ALTER TABLE dingdan
 	ADD FOREIGN KEY (user_id)
@@ -78,7 +115,7 @@ ALTER TABLE shopcar
 ;
 
 
-ALTER TABLE dingdan
+ALTER TABLE comments
 	ADD FOREIGN KEY (wine_id)
 	REFERENCES wine (wine_id)
 	ON UPDATE RESTRICT
--- src/com/getee/shopmanage/model/dao/BaseDAO.java
+++ src/com/getee/shopmanage/model/dao/BaseDAO.java
@@ -3,6 +3,6 @@ package com.getee.shopmanage.model.dao;
 public interface BaseDAO {
     public String dirverClass="com.mysql.jdbc.Driver";
     public String url="jdbc:mysql://rm-wz97w0rps07c1wrg0ao.mysql.rds.aliyuncs.com:3306?zeroDateTimeBehavior=convertToNull";
-    public String root="admin";
-    public String password="admin";
+    public String root="root";
+    public String password="root";
 }
